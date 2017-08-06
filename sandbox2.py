86011aec2c6bedd2c05631fd9eb565af59d608d7
Bella Wu <bella.wu02@gmail.com>
2017-08-06 09:28:17 -0700

    Remove clutter from sandbox2 so it runs to completion

--- sandbox2.py
+++ sandbox2.py
@@ -1,41 +1,38 @@
+#!/usr/bin/env python
+#
+# Usage: ./sandbox2.py -v
 
-a= input ("number1")
-b = input ("number2")
-print a + b
+import unittest
+import inspect
+import random
 
+def add_stdin_numbers():
+    a = input ("number1")
+    b = input ("number2")
+    return a + b
 
-a = input ( "your name is ")
-print (a + " " + "is a pig")
-
+def stdin_name_is_pig():
+    a = input ( "your name is ")
+    print (a + " " + "is a pig")
 
 a = ['aaa','c','dddd','rr']
-b= len(a[0])
-
-
-
-
+b = len(a[0])
 print b
-
 
 a = ['aaa','c','dddd','rr']
 sorted (a, key=len)
-
 print a
-
 
 def last(s):
     return s[-1]
 
-
-
-s= "hello"
+s = "hello"
 print s, last(s)
 
-
-def first(s):
+def first_two(s):
     return s[0:2]
 
-print s + " - " + first(s)
+print s + " - " + first_two(s)
 
 help(sorted)
 
@@ -44,9 +41,6 @@ sorted(a)
 
 print sorted(a), a
 
-
-
-
 def newkey(s):
     return s[1]
 
@@ -54,7 +48,6 @@ a= [('a',100),('b',93),('c',55)]
 #a.sort(a,key=newkey)
 
 print(sorted(a, key=newkey))
-
 
 def newkey(s):
     return s[1]
@@ -69,20 +62,16 @@ print(str(a)[1])
 
 print (50 - 5*6) / 4
 
-
 i = 256*256
 print('The value of i is ', i, 10, 78, "di")
 
 a, b = 1,1
 while b <10:
-    print(b, end=' ')
+    print b,
     a, b = a, b+a
 
 print()
 print (50 - 5*6) / 4
-
-
-
 
 i=1
 while i<5:
@@ -90,81 +79,82 @@ while i<5:
     i = i + 1
 print()
 
-"""
-words = ['zat', 'aindow', 'fefenestrate']
-
-
-
-for a in words[:]:
-    if len(a) < 8:
-        words.insert(0,a)
-
-print words
-print sorted( words)
-
-def newkey(s):
-    return s [1]
-
-print sorted(words, key=newkey)
-
-a = ['Mary', 'had', 'a', 'little', 'lamb']
-
-for i in range(len(a)):
-    print i, a[i]
-print
-
-print 'ab\nm'
-
-def one():
-    return 'hey'
-
-def two() :
-    print 'heyp'
-
-print 'hello'+ one()
-two()
-
-
-
-'''
-'''
-def bigger(a, b):
-    if a > b:
-        return a
-    elif a <b:
-        return b
-    else:
-        return a
-
-
-print bigger(1,3)
-
-
-def bigger(a, b):
-    if a > b:
-        return a
-    elif a <b:
-        print b
-    else:
-        print a
-
-
-bigger(1,3)
-
-
-def ret(n):
-    if n > 9:
-         temp = "two digits"
-         return temp     #Line 4
-    else :
-         temp = "one digit"
-         return temp     #Line 8
-    return     #Line 9
-    print ("return statement")
-print ret(10)
-
-
-''
-
-a= ['blue','hey','you']
-print a.join(' ')
+# """
+# words = ['zat', 'aindow', 'fefenestrate']
+#
+#
+#
+# for a in words[:]:
+#     if len(a) < 8:
+#         words.insert(0,a)
+#
+# print words
+# print sorted( words)
+#
+# def newkey(s):
+#     return s [1]
+#
+# print sorted(words, key=newkey)
+#
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+#
+# for i in range(len(a)):
+#     print i, a[i]
+# print
+#
+# print 'ab\nm'
+#
+# def one():
+#     return 'hey'
+#
+# def two() :
+#     print 'heyp'
+#
+# print 'hello'+ one()
+# two()
+#
+#
+#
+# '''
+# '''
+# def bigger(a, b):
+#     if a > b:
+#         return a
+#     elif a <b:
+#         return b
+#     else:
+#         return a
+#
+#
+# print bigger(1,3)
+#
+#
+# def bigger(a, b):
+#     if a > b:
+#         return a
+#     elif a <b:
+#         print b
+#     else:
+#         print a
+#
+#
+# bigger(1,3)
+#
+#
+# def ret(n):
+#     if n > 9:
+#          temp = "two digits"
+#          return temp     #Line 4
+#     else :
+#          temp = "one digit"
+#          return temp     #Line 8
+#     return     #Line 9
+#     print ("return statement")
+# print ret(10)
+#
+#
+# ''
+#
+# a= ['blue','hey','you']
+# print a.join(' ')
+# """
